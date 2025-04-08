b88c21cb7971c541b9d9b52f56967d3530ce59c4
Luis Llanes <luisllaboj@gmail.com>
2025-04-07 18:44:41 -0700

    chore: update theme sync

--- src/components/theme-sync.tsx
+++ src/components/theme-sync.tsx
@@ -5,15 +5,13 @@ import * as React from "react";
 import { useConfig } from "@/hooks/use-config";
 import { getCssVarsFromThemeObject } from "@/lib/themes";
 import { CssThemeProperties, ThemeMode } from "@/types/theme";
-import { setStyleProperty } from "@/utils/set-attribute-to-element";
+import {
+  DATA_KEYS,
+  DataKey,
+  setAttributeToElement,
+  setStyleProperty,
+} from "@/utils/set-attribute-to-element";
 import { useTheme } from "next-themes";
-
-const DATA_KEYS = {
-  preset: "data-preset",
-  primary: "data-primary",
-  surface: "data-surface",
-  variant: "data-variant",
-};
 
 export function ThemeSync() {
   const [config] = useConfig();
@@ -26,9 +24,23 @@ export function ThemeSync() {
 
     const preset = config.themeObject.name;
     const primary = config.themeObject[mode].primary;
+    const surface = config.surface ?? "default";
 
-    root.setAttribute(DATA_KEYS.preset, preset);
-    root.setAttribute(DATA_KEYS.primary, primary);
+    setAttributeToElement({
+      element: root,
+      attribute: DATA_KEYS.preset,
+      value: preset,
+    });
+    setAttributeToElement({
+      element: root,
+      attribute: DATA_KEYS.primary,
+      value: primary,
+    });
+    setAttributeToElement({
+      element: root,
+      attribute: DATA_KEYS.surface,
+      value: surface,
+    });
 
     const themeProperties: CssThemeProperties = {
       ...config.themeObject[mode],
@@ -38,7 +50,7 @@ export function ThemeSync() {
     const cssVars = getCssVarsFromThemeObject(themeProperties);
 
     for (const [key, value] of Object.entries(cssVars)) {
-      setStyleProperty({ element: root, key, value });
+      setStyleProperty({ element: root, key: key as DataKey, value });
     }
   }, [config, mode]);
 
--- src/utils/set-attribute-to-element.ts
+++ src/utils/set-attribute-to-element.ts
@@ -1,11 +1,46 @@
+export const DATA_KEYS = {
+  preset: "data-preset",
+  primary: "data-primary",
+  surface: "data-surface",
+  variant: "data-variant",
+} as const;
+
+export type DataKey = (typeof DATA_KEYS)[keyof typeof DATA_KEYS];
+
 export function setStyleProperty({
   element,
   value,
   key,
 }: {
   element: HTMLElement;
-  key: string;
+  key: DataKey;
   value: string;
 }) {
   element.style.setProperty(key, value);
+}
+
+export function setAttributeToElement({
+  element,
+  attribute,
+  value,
+}: {
+  element: HTMLElement;
+  attribute: DataKey;
+  value: string;
+}) {
+  if (element) {
+    element.setAttribute(attribute, value);
+  }
+}
+
+export function getAttributeFromElement({
+  element,
+  attribute,
+}: {
+  element: HTMLElement;
+  attribute: DataKey;
+}) {
+  if (element) {
+    return element.getAttribute(attribute);
+  }
 }
