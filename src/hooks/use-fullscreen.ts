ebb052da6210c475cbb80eaf3eb872ec051ae5c4
Luis Llanes <luisllaboj@gmail.com>
2025-04-19 23:55:16 -0700

    chore: remove fullscreen mode (f11) from browser

--- src/hooks/use-fullscreen.ts
+++ src/hooks/use-fullscreen.ts
@@ -1,26 +1,10 @@
-import { useState, useEffect } from "react";
-import screenfull from "screenfull";
+import { useState } from "react";
 
 export const useFullscreen = () => {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
-  useEffect(() => {
-    if (screenfull.isEnabled) {
-      const handleFullscreenChange = () => {
-        setIsFullscreen(screenfull.isFullscreen);
-      };
-
-      screenfull.on("change", handleFullscreenChange);
-      return () => {
-        screenfull.off("change", handleFullscreenChange);
-      };
-    }
-  }, []);
-
   const toggleFullscreen = () => {
-    if (screenfull.isEnabled) {
-      screenfull.toggle();
-    }
+    setIsFullscreen((prev) => !prev);
   };
 
   return {
