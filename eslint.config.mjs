0fc751a5a2a3eae070041d2e1c32e08ddf7ed346
Luis Llanes <luisllaboj@gmail.com>
2025-05-01 17:55:31 -0700

    deps: upgrade react-compiler plugins and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import * as reactHooks from "eslint-plugin-react-hooks";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,9 +12,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  reactHooks.configs.recommended,
   {
     rules: {
       "@next/next/no-html-link-for-pages": "off",
+      "react-hooks/react-compiler": "error",
     },
   },
 ];
